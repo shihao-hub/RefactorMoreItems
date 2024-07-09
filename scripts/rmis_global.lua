236dfd6dcd219a0339a5584272ef64e438c2ccc6
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2024-07-09 21:42:03 +0800

    Update rmis_global.lua

--- scripts/rmis_global.lua
+++ scripts/rmis_global.lua
@@ -1,12 +1,24 @@
-local g = {}
+local lb = require("_rmis_lua_builtins")
+local g = lb
 
-function g.map(fn, iter)
-    local res = {}
-    local n = table.maxn(iter)
-    for i = 1, n do
-        table.insert(res, fn(iter[i]))
+function g.pairs_by_keys(t, cmp)
+    local keys = lb.keys(t)
+    table.sort(keys, cmp)
+    local i = 0
+    return function()
+        i = i + 1
+        return keys[i], t[keys[i]]
     end
-    return res
+end
+
+if debug.getinfo(3) == nil then
+    --for i in b.range(10, 0, -1) do
+    --    print(i)
+    --end
+
+    for k, v in g.pairs_by_keys({ b = 2, c = 3, a = 1, ba = 21 }) do
+        print(k, v)
+    end
 end
 
 return g
