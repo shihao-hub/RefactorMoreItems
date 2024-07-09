e38b9b7005a4c8d2cf96c70eee336c1cdb9cbd17
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2024-07-09 21:41:09 +0800

    Create _rmis_python_builtins.lua

---
+++ scripts/_rmis_python_builtins.lua
@@ -0,0 +1,107 @@
+local b = require("_rmis_base_builtins")
+local pb = b
+
+--- Return True if bool(x) is True for all values x in the iterable.
+--- If the iterable is empty, return True.
+---@param iterable table<any>
+---@return boolean
+function pb.all(iterable)
+    for i = 1, table.maxn(iterable) do
+        if not iterable[i] then
+            return false
+        end
+    end
+    return true
+end
+
+--- Return True if bool(x) is True for any x in the iterable.
+--- If the iterable is empty, return False.
+---@param iterable table<any>
+---@return boolean
+function pb.any(iterable)
+    for i = 1, table.maxn(iterable) do
+        if iterable[i] then
+            return true
+        end
+    end
+    return false
+end
+
+--- Make an iterator that computes the function using arguments from
+--- each of the iterables.  Stops when the shortest iterable is exhausted.
+---@param fn function
+---@param iterable table<any>
+---@return table<any>
+function pb.map(fn, iterable)
+    local res = {}
+    local n = table.maxn(iterable)
+    for i = 1, n do
+        table.insert(res, fn(iterable[i]))
+    end
+    return res
+end
+
+--- python filter
+function pb.filter(fn, iter)
+    -- If function is None, return the items that are true.
+    fn = fn or function(v)
+        return v
+    end
+    local res = {}
+    local n = table.maxn(iter)
+    for i = 1, n do
+        if fn(iter[i]) then
+            table.insert(res, iter[i])
+        end
+    end
+    return res
+end
+
+--- python range
+--- 但有些区别，Python 是 [)，由于 Lua 从 1 开始，所以 Lua 用 [] 比较好？
+--- 还是需要考虑考虑的
+function pb.range(start, stop, step)
+    if start and not stop then
+        stop = start
+        start = 1
+    end
+    step = step or 1
+    return function()
+        if step > 0 and start >= stop or step < 0 and start <= stop then
+            return nil
+        end
+        local res = start
+        start = start + step
+        return res
+    end
+end
+
+function pb.sorted(iterable, key, reverse)
+    b.not_implemented_error()
+end
+
+--- 试图模仿 python 的 f
+function pb.f(formatstring, data)
+    local function string_strip(v)
+        return string.gsub(v, "^ *(.-) *$", "%1")
+    end
+
+    return (string.gsub(formatstring, "{{ *([A-Za-z0-9_]*) *}}", function(v)
+        -- 捕获到的内容是这个 v，需要注意到是，没匹配到不会进入这个函数。所以要注意...
+        if not data[v] then
+            error("data 中不存在 " .. v .. " 这个键", 4)
+        end
+        return string.format("%s", tostring(data[v]))
+    end))
+end
+
+if debug.getinfo(3) == nil then
+    print(pb.any({ 1, nil, 3 }))
+    print(pb.all({ 1, 3, 3, nil }))
+    print(b.list_tostring(pb.filter(function(x)
+        return x > 10
+    end, { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 })))
+
+end
+
+return pb
