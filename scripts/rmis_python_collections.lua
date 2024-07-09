46df086d9caa9406c447b951547ed470aefd8eb1
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2024-07-09 21:42:16 +0800

    Update rmis_python_collections.lua

--- scripts/rmis_python_collections.lua
+++ scripts/rmis_python_collections.lua
@@ -1,5 +1,5 @@
 local class = require("rmis_middleclass")
-local str = require("rmis_string")
+local g = require("rmis_global")
 
 local function namedtuple(typename, field_names)
     local cls = class(typename)
@@ -13,22 +13,25 @@ local function namedtuple(typename, field_names)
 
         local function loop_body(v)
             if self[v] then
-                error(str.f("self[{v}] 已经存在", { v = v }))
+                error(g.f("self[{{ v }}] 已经存在", { v = v }))
             end
             self[v] = args[i]
             i = i + 1
         end
 
-        if type(field_names) == "string" then
+        local is_string = type(field_names) == "string"
+        local is_table =  type(field_names) == "table"
+
+        if is_string then
             for v in string.gmatch(field_names, "[^ ]+") do
                 loop_body(v)
             end
-        elseif type(field_names) == "table" then
+        elseif is_table then
             for _, v in ipairs(field_names) do
                 loop_body(v)
             end
         else
-            error(str.f("field_names 的类型为 {{ type(field_names) }}，而不是 string 和 table", {
+            error(g.f("field_names 的类型为 {{ type(field_names) }}，而不是 string 和 table", {
                 ["type(field_names)"] = type(field_names)
             }))
         end
@@ -40,12 +43,12 @@ local function namedtuple(typename, field_names)
 end
 
 if debug.getinfo(3) == nil then
-    local StrNt = namedtuple("StrNt", 1)
+    local StrNt = namedtuple("StrNt", {"name","id","initialize"})
     local snt = StrNt("dog", 123)
     print(snt.name, snt.id)
 
-    ---- middleclass 返回的 StrNt 是张普通表，或者说他的元表并没有实现 __call 方法
-    ---- 吐槽一下，middleclass 的用法作者都没写，还是得找机会看一下源码...好像只支持单继承吧
+    -- middleclass 返回的 StrNt 是张普通表，或者说他的元表并没有实现 __call 方法
+    -- 吐槽一下，middleclass 的用法作者都没写，还是得找机会看一下源码...好像只支持单继承吧
     --print(getmetatable(StrNt))
     --local snt = StrNt("dog", 123)
     --local StrNt = class("StrNt",namedtuple("name id"))
