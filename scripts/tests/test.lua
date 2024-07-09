5b7876075ce0348c52b031f96d8b3c5c623dc1e8
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2024-07-09 21:38:46 +0800

    Update test.lua

--- scripts/tests/test.lua
+++ scripts/tests/test.lua
@@ -8,3 +8,10 @@ local str = require("rmis_string")
 --print(debug.getinfo(1).short_src)
 --print(debug.getinfo(2).short_src)
 require("rmis_class_stringbuilder")
+
+print(string.match("word|word|word","(%w+)()(|)()"))
+
+
+print(string.find("self.len+1","([ ()-A-Za-z0-9_]+)"))
+
+print(string.find("   self.len + 1 ","^ *(.-) *$"))
