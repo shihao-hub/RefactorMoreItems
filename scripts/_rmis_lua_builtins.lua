072beadeed1bfd20b898812c742d901de7f0ccaa
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2024-07-11 19:53:07 +0800

    Update _rmis_lua_builtins.lua

--- scripts/_rmis_lua_builtins.lua
+++ scripts/_rmis_lua_builtins.lua
@@ -26,11 +26,30 @@ function lb.contain(t, val)
     return false
 end
 
+--- 此处比 lua 的 assert 多了一个 level 参数
+---@overload fun(v:boolean)
+---@overload fun(v:boolean,level:number)
+---@param v boolean
+---@param message string
+---@param level number
+function lb.assert(v, message, level)
+    -- 注意，此处重载了三次函数，与其考虑 if else 的情况，不如直接三个分支，哪怕有重复代码...
+    if type(message) == "number" and not level then
+        level = message + 1
+        message = "assertion failed!"
+    else
+        message = message or "assertion failed!"
+        level = level and level + 1 or 2
+    end
+    if not v then
+        error(message, level)
+    end
+end
 
 if debug.getinfo(3) == nil then
     print(lb.any({ 1, nil, 3 }))
     print(lb.all({ 1, 3, 3, nil }))
-
+    lb.assert(false, 1)
 
 end
 
