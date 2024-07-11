13ede8bd8c587aaef75709bec6250b66ecb5d971
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2024-07-11 19:53:53 +0800

    Update rmis_class_vector.lua

--- scripts/rmis_class_vector.lua
+++ scripts/rmis_class_vector.lua
@@ -1,16 +1,31 @@
 local g = require("rmis_global")
 local str = require("rmis_string")
-
 local Vector = require("_rmis_class_vector_local")
 
 --local DEFAULT_CAPACITY = 3 -- Lua 表会自动扩容，所以不需要这个默认容量
 
 local SORTED_CATEGORY = { "ASC", "DES" }
 
-Vector.static.__apis__ = { "size", "get", "put", "insert", "remove", "disordered", "sort", "find", "search", "deduplicate", "uniquify", "traverse" }
+Vector.static.__apis__ = {
+    "size",
+    "empty",
+    "get",
+    "put",
+    "insert",
+    "remove",
+    "disordered",
+    "sort",
+    "find",
+    "search",
+    "deduplicate",
+    "uniquify",
+    "traverse"
+}
 
 ---@overload fun()
 function Vector:initialize(scale, init_val)
+    --Vector.super:initialize(scale, init_val)
+    --print(self)
 
     if scale then
         self:_check_non_nil_value(init_val)
@@ -18,37 +33,42 @@ function Vector:initialize(scale, init_val)
 
     scale = scale or 0
 
-    self.len = scale
+    self._size = scale
     self.data = {}
-    for i in g.range(scale + 1) do
+    for i in g.range(g.right_open(scale)) do
         self.data[i] = init_val
     end
 
-    self.sored_category = nil -- ?
+    self.sored_category = nil -- future
 end
 
 function Vector:size()
-    return self.len
+    return self._size
+end
+
+function Vector:empty()
+    return self._size == 0
 end
 
 function Vector:get(r)
-    self:_check_valid_rank_value(r)
+    self:_check_valid_rank_value(r, 1, g.right_open(self._size))
+
     return self.data[r]
 end
 
 function Vector:put(r, e)
     self:_check_non_nil_value(e, "不允许赋值为 nil")
-    self:_check_valid_rank_value(r, nil, 1, self.len + 1)
+    self:_check_valid_rank_value(r, nil, 1, g.right_open(self._size))
 
     self.data[r] = e
-    self.len = self.len + 1
+    self._size = self._size + 1
 end
 
 --- 查找等于 e 且秩最大的元素，[lo, hi)
 ---@overload fun(e:any):boolean
 function Vector:find(e, lo, hi)
     lo = lo or 1
-    hi = hi or self.len - 1
+    hi = hi or self._size + 1
     for i in g.range(hi - 1, lo - 1, -1) do
         if self.data[i] == e then
             return true
@@ -80,17 +100,17 @@ end
 function Vector:insert(r, e)
     if r and not e then
         e = r
-        r = self.len + 1
+        r = self._size + 1
     end
 
     self:_check_non_nil_value(e, "插入值不允许为 nil")
-    self:_check_valid_rank_value(r, nil, 1, self.len + 1)
+    self:_check_valid_rank_value(r, nil, 1, g.right_open(self._size) + 1)
 
-    for i in g.range(self.len + 1, r, -1) do
+    for i in g.range(self._size + 1, r, -1) do
         self.data[i] = self.data[i - 1]
     end
     self.data[r] = e
-    self.len = self.len + 1
+    self._size = self._size + 1
     return self
 end
 
@@ -103,29 +123,24 @@ end
 --- 删除区间的时候与 C++ 匹配，[lo,hi)，保证统一！
 ---@return number 返回被删除元素的数目
 function Vector:remove_range(lo, hi)
-    assert(lo < hi and lo >= 1 and hi < self.len + 1, "assert: lo < hi and lo >= 1 and hi < self.len + 1")
+    -- 只能删除 [1,len]，左闭右开，那么应该只能输入 [1,len+1)
+    assert(lo < hi and lo >= 1 and hi <= g.right_open(self._size),
+            g.f("输入的区间 [{{lo}}, {{hi}}) 不是 [1, {{right}}) 的子集", { lo = lo, hi = hi, right = g.right_open(self._size)}))
 
     -- 方法一
     --local interval_len = hi - lo + 1
-    --for i in g.range(lo, self.len - interval_len + 1) do
+    --for i in g.range(lo, self._size - interval_len + 1) do
     --    self.data[i] = self.data[i + interval_len]
     --end
-    --self.len = self.len - interval_len
+    --self._size = self._size - interval_len
 
-    -- 方法二，要注意这个，Lua 写久了 C 思维会退化的
-    while hi < self.len + 1 do
+    -- 方法二，要注意这个，Lua 写久了 C 思维会退化的（方法一也还行，但是不是 C++ 风格，应该是 Java 风格）
+    while hi < g.right_open(self._size) do
         self.data[lo] = self.data[hi]
         lo = lo + 1
         hi = hi + 1
     end
-    --self.len = lo - 1
-    self:_shrink_the_interval(lo, self.len)
-
-    -- 注意：这个不需要，有 self.len 限制就行了！
-    --for i in g.range(self.len + 1, old_len + 1) do
-    --    self.data[i] = nil
-    --end
-
+    self:_shrink_the_interval(lo, g.right_open(self._size))
     return hi - lo
 end
 
@@ -133,8 +148,8 @@ end
 --- 有序向量不要求元素互异，通常约定其中的元素自前向后构成一个非降序列
 --- 目前假设元素都可以比较大小
 function Vector:disordered()
-    for i in g.range(self.len) do
-        if self.data[i] > self.data[i + 1] then
+    for i in g.range(2, g.right_open(self._size)) do
+        if self.data[i - 1] > self.data[i] then
             return true
         end
     end
@@ -149,7 +164,7 @@ function Vector:uniquify()
 
     -- 时间复杂度高达 O(n^2)
     --local removed_index = {}
-    --for i in g.range(2, self.len + 1) do
+    --for i in g.range(2, self._size + 1) do
     --    if self.data[i] == self.data[i - 1] then
     --        table.insert(removed_index, i)
     --    end
@@ -158,15 +173,15 @@ function Vector:uniquify()
     --    self:remove(removed_index[i] - i + 1) -- 调用删除操作的时候索引会变动，所以既然如此，此处的代码显然可以放入上面的循环中。
     --end
 
-    -- 改进一下，但是还不能算最优
-    for i in g.range(2, self.len + 1) do
+    -- 改进一下，但是还不能算最优。因为这里不是书中的实现，是我自己的实现
+    for i in g.range(2, g.right_open(self._size)) do
         if self.data[i] == self.data[i - 1] then
             self.data[i - 1] = nil -- 注意，是 i-1
         end
     end
 
     local i, j = 1, 1
-    local old_len = self.len
+    local old_len = self._size
     while j <= old_len do
         if self.data[j] then
             self.data[i] = self.data[j]
@@ -174,8 +189,8 @@ function Vector:uniquify()
         end
         j = j + 1
     end
-    --self.len = i - 1 -- 直接截除后面的内容，但是说实在的，不太好，因为并没有实现 shrink 缩容函数，因为不需要扩容，如果这样的话，截除并不合适
-    self:_shrink_the_interval(i, self.len)
+    --self._size = i - 1 -- 直接截除后面的内容，但是说实在的，不太好，因为并没有实现 shrink 缩容函数，因为不需要扩容，如果这样的话，截除并不合适
+    self:_shrink_the_interval(i, g.right_open(self._size))
     i, j = nil, nil
 end
 
@@ -191,7 +206,7 @@ function Vector:traverse(fn)
 end
 
 function Vector:__tostring()
-    return g.list_tostring(self.data, 1, self.len)
+    return g.list_tostring(self.data, 1, self._size)
 end
 
 if debug.getinfo(3) == nil then
@@ -213,9 +228,11 @@ if debug.getinfo(3) == nil then
     --vec:put(10, 6)
     vec:insert(1):insert(2):insert(3):insert(3):insert(3):insert(4):insert(5):insert(6):insert(7):insert(8):insert(9):insert(9):insert(9):insert(10)
     print(vec)
-    vec:uniquify()
+    print(vec:size())
+    vec:remove_range(1, 16)
     print(vec)
-    print(vec:tostring())
+    --vec:uniquify()
+    --print(vec)
 
     --print(vec:remove(8))
     --print(vec)
