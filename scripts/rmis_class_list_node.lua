1acc4059bbf51d4882d265e1d2d4e5ff72369883
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2024-07-11 19:53:29 +0800

    Update rmis_class_list_node.lua

--- scripts/rmis_class_list_node.lua
+++ scripts/rmis_class_list_node.lua
@@ -1,53 +1,36 @@
 local g = require("rmis_global")
 local ListNode = require("_rmis_class_list_node_local")
 
-ListNode.static.__apis__ = { "insert_as_pred", "insert_as_succ", "size", "first", "last", "insert_as_first", "insert_as_last" }
+ListNode.static.__apis__ = {
+    "insert_as_pred",
+    "insert_as_succ",
+}
 
----@param data any
----@param pred ListNode
----@param succ ListNode
+---@param data any|nil
+---@param pred ListNode|nil
+---@param succ ListNode|nil
 function ListNode:initialize(data, pred, succ)
     self.data = data
-    self.pred = pred or nil
-    self.succ = succ or nil
+    self.pred = pred
+    self.succ = succ
 end
 
-function ListNode:size()
-
+--- 插入前驱节点，存入数据，返回新节点
+---@return ListNode
+function ListNode:insert_as_pred(data)
+    local node = ListNode(data, self.pred, self)
+    self.pred.succ = node
+    self.pred = node
+    return node
 end
 
---- 删除位置 p 处的节点，返回其数值
----@param p ListNode
-function ListNode:remove(p)
-
-end
-
-function ListNode:size()
-
-end
-
----@param e ListNode
-function ListNode:insert_as_first(e)
-
-end
-
----@param e ListNode
-function ListNode:insert_as_last(e)
-
-end
-
---- 紧靠当前节点之前插入新节点
----@param p ListNode
----@param e ListNode
-function ListNode:insert_as_pred(p, e)
-
-end
-
---- 紧靠当前节点之后插入新节点
----@param p ListNode
----@param e ListNode
-function ListNode:insert_as_succ(p, e)
-
+--- 插入后继节点，存入数据，返回新节点
+---@return ListNode
+function ListNode:insert_as_succ(data)
+    local node = ListNode(data, self, self.succ)
+    self.succ.pred = node
+    self.succ = node
+    return node
 end
 
 return ListNode
