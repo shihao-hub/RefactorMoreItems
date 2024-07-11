79bd7b46680c3bbff21f361bfdcdbc8227ab2cc1
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2024-07-11 19:52:21 +0800

    Update _rmis_class_list_node_local.lua

--- scripts/_rmis_class_list_node_local.lua
+++ scripts/_rmis_class_list_node_local.lua
@@ -1,8 +1,8 @@
+local g = require("rmis_global")
 local class = require("rmis_middleclass")
 local Object = require("rmis_class_object")
 
 ---@class ListNode : Object
-local ListNode = class("ListNode")
-
+local ListNode = class("ListNode",Object)
 
 return ListNode
