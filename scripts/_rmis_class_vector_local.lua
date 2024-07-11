0b8ab5b6f2efac71e46e90aedd68c5491ddf3d3b
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2024-07-11 19:52:56 +0800

    Update _rmis_class_vector_local.lua

--- scripts/_rmis_class_vector_local.lua
+++ scripts/_rmis_class_vector_local.lua
@@ -17,21 +17,32 @@ function Vector:_check_non_nil_value(v, msg)
     end
 end
 
+--- [)
+---@overload fun(left:number,right:number)
 function Vector:_check_valid_rank_value(r, msg, left, right)
-    msg = msg or g.f("秩超出范围了，合法范围为：[{{l}}, {{r}}]", { l = left, r = right })
-    left = left or 1
-    right = right or self.len
-    if not (r >= left and r <= right) then
+    if not msg and not left and not right then
+        left = 1
+        right = self._size
+    elseif msg and left and not right then
+        right = left
+        left = msg
+    end
+    msg = g.f("秩超出范围了，合法范围为：[{{l}}, {{r}})", { l = left, r = right })
+    if not (r >= left and r < right) then
         error(msg, 2)
     end
 end
 
---- 收缩一下区间，不用的内容设置为 nil，方便 Lua 自己找机会收缩
+--- 收缩一下区间，不用的内容设置为 nil，方便 Lua 自己找时间清理掉
 function Vector:_shrink_the_interval(left, right)
     for i in g.range(left, right) do
         self.data[i] = nil
     end
-    self.len = left - 1
+    self._size = left - 1
+end
+
+function Vector:_tostring(left_char, right_char)
+
 end
 
 return Vector
