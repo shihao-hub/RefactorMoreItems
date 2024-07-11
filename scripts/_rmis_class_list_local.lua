b6115aaf5ebf534465e44633514af72565aa8603
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2024-07-11 19:49:18 +0800

    Create _rmis_class_list_local.lua

---
+++ scripts/_rmis_class_list_local.lua
@@ -0,0 +1,35 @@
+local g = require("rmis_global")
+local class = require("rmis_middleclass")
+local Object = require("rmis_class_object")
+
+---@class List : Object
+local List = class("List", Object)
+
+function List:_insertion_sort(p, n)
+
+end
+
+function List:_selection_sort(p, n)
+
+end
+
+function List:_merer_sort(p, n)
+
+end
+
+function List:_tostring(left_char, right_char)
+    local res = {}
+    table.insert(res, left_char)
+    local p = self.header
+    while true do
+        p = p.succ
+        if p == self.trailer then
+            break
+        end
+        table.insert(res, g.f("{{data}}" .. (p.succ ~= self.trailer and ", " or ""), { data = p.data }))
+    end
+    table.insert(res, right_char)
+    return table.concat(res)
+end
+
+return List
