87cc111c935fc0b2362b707e324f17460c9b89a4
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2024-07-16 20:26:59 +0800

    Create _rmis_class_bi_tree_local.lua

---
+++ scripts/_rmis_class_bi_tree_local.lua
@@ -0,0 +1,8 @@
+local g = require("rmis_global")
+local class = require("rmis_middleclass")
+local Object = require("rmis_class_object")
+
+---@class BiTree : Object
+local BiTree = class("BiTree",Object)
+
+return BiTree
