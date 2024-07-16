6c6b348b57f05954c4728929c739e4bd0e6b6d1e
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2024-07-16 20:28:47 +0800

    Update rmis_class_list_node.lua

--- scripts/rmis_class_list_node.lua
+++ scripts/rmis_class_list_node.lua
@@ -3,8 +3,8 @@ local str = require("rmis_string")
 local ListNode = require("_rmis_class_list_node_local")
 
 ListNode.static.__apis__ = {
-    "insert_as_pred",
-    "insert_as_succ",
+    "insert_as_pred", -- 插入前驱（插入前不允许有前驱）
+    "insert_as_succ", -- 插入后继（插入前不允许有后驱）
 }
 
 ---@param data any|nil
@@ -19,6 +19,8 @@ end
 --- 插入前驱节点，存入数据，返回新节点
 ---@return ListNode
 function ListNode:insert_as_pred(data)
+    g.assert(not self.pred, "当前节点已有前驱，不允许插入", 2)
+
     local node = ListNode(data, self.pred, self)
     self.pred.succ = node
     self.pred = node
@@ -28,6 +30,8 @@ end
 --- 插入后继节点，存入数据，返回新节点
 ---@return ListNode
 function ListNode:insert_as_succ(data)
+    g.assert(not self.succ, "当前节点已有后继，不允许插入", 2)
+
     local node = ListNode(data, self, self.succ)
     self.succ.pred = node
     self.succ = node
