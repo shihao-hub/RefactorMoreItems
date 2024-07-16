51cd8b5f183fbb5d2272bfbe17905300f09a3a80
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2024-07-16 20:28:58 +0800

    Update rmis_class_object.lua

--- scripts/rmis_class_object.lua
+++ scripts/rmis_class_object.lua
@@ -1,6 +1,15 @@
 local g = require("rmis_global")
 local Object = require("_rmis_class_object_local")
 
+--- 注意，这个 hash 不一样，如果类就返回地址，如果是其他，则返回它自身
+function Object.hash_object(obj)
+    return obj and (obj.hash and obj:hash()) or obj
+end
+
+function Object.tostring_object(obj)
+    return obj and (obj.hash and obj:hash()) or obj
+end
+
 function Object:tostring()
     g.subclass_responsibility_error()
 end
@@ -13,6 +22,15 @@ end
 --- 默认是比较地址，所以需要重写
 function Object:equals(obj)
     return self == obj
+end
+
+function Object:hash()
+    local meta = getmetatable(self)
+    local old = meta.__tostring
+    meta.__tostring = nil
+    local res = string.sub(tostring(self),8)
+    meta.__tostring = old
+    return res
 end
 
 if debug.getinfo(3) == nil then
