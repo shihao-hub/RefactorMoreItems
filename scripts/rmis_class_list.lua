8e13bdfb039d5362b5800c60b355f9c78ce36e63
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2024-07-16 20:28:36 +0800

    Update rmis_class_list.lua

--- scripts/rmis_class_list.lua
+++ scripts/rmis_class_list.lua
@@ -4,21 +4,25 @@ local ListNode = require("rmis_class_list_node")
 local List = require("_rmis_class_list_local")
 
 List.static.__apis__ = {
-    "size",
-    "first",
-    "last",
-    "insert_as_first",
-    "insert_as_last",
-    "insert_before",
-    "insert_after",
-    "remove",
-    "disordered",
-    "sort",
-    "find",
-    "search",
-    "deduplicate",
-    "uniquify",
-    "traverse",
+    "size", -- 节点总数
+    "empty", -- 是否为空
+
+    "first", -- 首节点的位置（可能为 trailer）
+    "last", -- 尾节点的位置（可能为 header）
+    "insert_as_first", -- 头插法
+    "insert_as_last", -- 尾插法
+    "insert_before", -- 插到某个节点的前面
+    "insert_after", -- 插到某个节点的后面
+
+    "remove", -- 删除某个节点
+    "sort", -- 非降序排序
+    "find", -- 视序列为乱序，查找指定元素
+    "disordered", -- 是否为乱序
+    "search", -- 视序列为非降序，查找目标元素，返回不大于目标元素且秩最大的节点（等价于逆序查找时第一个满足条件的元素）
+    "deduplicate", -- 视序列为乱序，剔除重复节点
+    "uniquify", -- 视序列为非降序，剔除重复节点
+
+    "traverse", -- 这个的定义有待商榷
 }
 
 function List:initialize()
@@ -38,10 +42,12 @@ function List:empty()
     return self._size == 0
 end
 
+-- FIXME
 function List:first()
     return self.header.succ
 end
 
+-- FIXME
 function List:last()
     return self.trailer.pred
 end
@@ -102,6 +108,8 @@ end
 ---@param p ListNode
 ---@param n number
 function List:sort(p, n)
+    g.not_implemented_error()
+
     if not p and not n then
         p = self:first()
         n = self:size()
@@ -123,12 +131,12 @@ function List:sort(p, n)
     return switch[rand]
 end
 
---- 节点 p 的 n 个真前驱中，找到等于 e 的最后者
----@param e any
+--- 节点 p 的 n 个真前驱中，找到等于 data 的最后者
+---@param data any
 ---@param n number
 ---@param p ListNode
 ---@return ListNode|nil
-function List:find(e, n, p)
+function List:find(data, n, p)
     local cur = p
     local res
     -- 用区间的左开右闭类似的思想确定边界值很舒服
@@ -137,24 +145,24 @@ function List:find(e, n, p)
         if cur == self.header then
             break
         end
-        if cur.data == e then
+        if cur.data == data then
             res = cur
         end
     end
     return res
 end
 
---- 在有序列表内节点 p 的 n 个真前驱中（所以不考虑 p 自己），找到不大于 e 的最后者
----@param e any
+--- 在有序列表内节点 p 的 n 个真前驱中（所以不考虑 p 自己），找到不大于 data 的最后者
+---@param data any
 ---@param n number
 ---@param p ListNode
 ---@return ListNode|nil
-function List:search(e, n, p)
-    if self:disordered() then
-        error("该函数只适用于有序链表")
-    end
-
+function List:search(data, n, p)
     g.untested_error()
+
+    if self:disordered() then
+        error("该函数只适用于有序链表", 2)
+    end
 
     local cur = p
     for _ in g.range(n, 0, -1) do
@@ -162,7 +170,7 @@ function List:search(e, n, p)
         if cur == self.header then
             break
         end
-        if cur.data <= e then
+        if cur.data <= data then
             return cur -- 最后者是只从右往左第一个？
         end
     end
@@ -170,18 +178,20 @@ function List:search(e, n, p)
 end
 
 function List:deduplicate()
-
+    g.not_implemented_error("List:deduplicate()")
 end
 
 function List:uniquify()
-    if self:disordered() then
-        error("该函数只适用于有序链表")
-    end
+    g.not_implemented_error("List:uniquify()")
 
+    if self:disordered() then
+        error("该函数只适用于有序链表", 2)
+    end
 end
 
 function List:traverse(fn)
-    g.not_implemented_error()
+    g.not_implemented_error("List:traverse(fn)")
+
     print("WARNING: 当前调用的函数会修改元素本身，请注意")
     local generator = g.iter(self)
     return function()
@@ -194,6 +204,7 @@ function List:traverse(fn)
     end
 end
 
+-- TODO：深入 Python，把 Python 优秀的东西用 Lua 模拟实现一下！尤其鸭子类型！
 function List:__iter__()
     local p = self.header
     return function()
@@ -227,6 +238,7 @@ if debug.getinfo(3) == nil then
     print(list:last().data)
     print(list:find(3, 10, list:last()).data)
     print(list:disordered())
+    list:deduplicate()
     --for e in g.iter(require("rmis_class_vector")()) do
     --    print("--", e)
     --end
