e7ce625f7fac137bbfe999f206264058d871da7e
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2024-08-01 19:36:22 +0800

    Create _rmis_class_dictionary_local.lua

---
+++ scripts/_rmis_class_dictionary_local.lua
@@ -0,0 +1,9 @@
+local g = require("rmis_global")
+local class =require("rmis_middleclass")
+local Object = require("rmis_class_object")
+
+
+---@class Dictionary : Object
+local Dict = class("Dict",Object)
+
+return Dict
